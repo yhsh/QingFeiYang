61fde3c4ce7dcc152c4ebbb71732af79e7bf96f9
zhengcong <1722217676@qq.com>
2018-01-16 16:22:12 +0800

    1.新增主题切换 2.新增关于页面

---
+++ YHSH/src/main/java/com/xiayiye/yhsh/yhsh/AboutYHSHActivity.java
@@ -0,0 +1,13 @@
+package com.xiayiye.yhsh.yhsh;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutYHSHActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_yhsh);
+    }
+}
--- YHSH/src/main/java/com/xiayiye/yhsh/yhsh/HomeActivity.java
+++ YHSH/src/main/java/com/xiayiye/yhsh/yhsh/HomeActivity.java
@@ -9,11 +9,16 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.support.v4.content.IntentCompat;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.xiayiye.yhsh.yhsh.api.YhshAPI;
 import com.xiayiye.yhsh.yhsh.fragment.FourFragment;
@@ -40,6 +45,10 @@ public class HomeActivity extends Activity {
     };
     private TextView home_tv_pmd;
     private String history_today = "";
+    private RadioButton rb_move;
+    private RadioButton rb_news;
+    private RadioButton rb_three;
+    private RadioButton rb_four;
 
     private void initJson(String str) {
         try {
@@ -60,16 +69,17 @@ public class HomeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        onActivityCreateSetTheme(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         getFragmentManager().beginTransaction().replace(R.id.fl_replace, new MoveFragment()).commit();
         FrameLayout fl_replace = findViewById(R.id.fl_replace);
         home_tv_pmd = findViewById(R.id.home_tv_pmd);
         RadioGroup rg_bottom = findViewById(R.id.rg_bottom);
-        RadioButton rb_one = findViewById(R.id.rb_move);
-        RadioButton rb_two = findViewById(R.id.rb_news);
-        RadioButton rb_three = findViewById(R.id.rb_three);
-        RadioButton rb_four = findViewById(R.id.rb_four);
+        rb_move = findViewById(R.id.rb_move);
+        rb_news = findViewById(R.id.rb_news);
+        rb_three = findViewById(R.id.rb_three);
+        rb_four = findViewById(R.id.rb_four);
         home_tv_pmd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -104,5 +114,66 @@ public class HomeActivity extends Activity {
         });
         ProgressDialog pd = ProgressDialog.show(this, "", "");
         GetNetworkJsonData.TakeNetworkData(YhshAPI.HISTORY_TODAY, handler, 0, pd, this, "UTF-8");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home_main_default:
+                //切换默认主题
+                changeToTheme(HomeActivity.this, 1);
+                break;
+            case R.id.home_main_night:
+                //切换夜间主题
+                changeToTheme(HomeActivity.this, 2);
+                rb_move.setTextColor(getResources().getColor(R.color.dodgerblue));
+                rb_news.setTextColor(getResources().getColor(R.color.dodgerblue));
+                rb_three.setTextColor(getResources().getColor(R.color.dodgerblue));
+                rb_four.setTextColor(getResources().getColor(R.color.dodgerblue));
+                break;
+            case R.id.home_main_light:
+                //切换白天主题
+                changeToTheme(HomeActivity.this, 3);
+                break;
+            case R.id.home_main_about:
+                //跳转到关于页面
+                startActivity(new Intent(this, AboutYHSHActivity.class));
+                break;
+        }
+        return true;
+    }
+
+    /* @Override
+     public boolean onMenuItemSelected(int featureId, MenuItem item) {
+         return super.onMenuItemSelected(featureId, item);
+     }*/
+    private static int themes;
+
+    public static void changeToTheme(Activity activity, int theme) {
+        themes = theme;
+        activity.finish();
+        activity.startActivity(new Intent(activity, activity.getClass()));
+        //通过下面的一个动画基本看不出来页面跳转
+        activity.overridePendingTransition(0, 0);
+    }
+
+    public void onActivityCreateSetTheme(Activity activity) {
+        switch (themes) {
+            case 1:
+                activity.setTheme(R.style.AppTheme);
+                break;
+            case 2:
+                activity.setTheme(R.style.ThemeNight);
+                break;
+            case 3:
+                activity.setTheme(R.style.themeLight);
+                break;
+        }
     }
 }
